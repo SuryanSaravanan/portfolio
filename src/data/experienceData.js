56b4ed00b21c3c125302da6dc1301c1bedf150a9
Suryan <suryansaravanan@gmail.com>
2025-05-17 19:24:00 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,13 +1,20 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'AppViewX',
-        jobtitle: 'Senior Software Engineer',
-        startYear: 'Nov 2019',
+        company: 'Wipro',
+        jobtitle: 'Senior Software Engineer II',
+        startYear: 'Nov 2023',
         endYear: 'Present'
     },
     {
         id: 2,
+        company: 'AppViewX',
+        jobtitle: 'Senior Software Engineer',
+        startYear: 'Nov 2019',
+        endYear: 'Oct 2023'
+    },
+    {
+        id: 3,
         company: 'AppViewX',
         jobtitle: 'Python Developer Intern',
         startYear: 'Jun 2019',
